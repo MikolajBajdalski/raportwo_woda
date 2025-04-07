c7d698cdce73e9795d29ee05671312f10d8e8151
MikolajBajdalski <bajdalm033@gmail.com>
2025-04-07 22:08:43 +0200

    wtf

--- generatorpdf.js
+++ generatorpdf.js
@@ -255,10 +255,10 @@ async function generatePDF(download = true) {
                 // ],
               ],
             },
-            margin: [0, 10, 0, 20],
+            // margin: [0, 0, 0, 0],
           },
           {
-            margin: [0, 10, 0, 0],
+            // margin: [0, 0, 0, 0],
             columns: [
               {
                 width: "100%",
@@ -272,7 +272,7 @@ async function generatePDF(download = true) {
             ],
           },
           {
-            margin: [0, 0, 0, 0], // odstęp od poprzedniego elementu
+            // margin: [0, 0, 0, 0],
             columns: [
               // {
               //   width: "50%",
@@ -296,7 +296,7 @@ async function generatePDF(download = true) {
                 width: "100%",
                 stack: [
                   // Podpis prawy
-                  { text: "", margin: [0, 0, 0, 0] },
+                  // { text: "", margin: [0, 0, 0, 0] },
                   {
                     text: "....................................................................................",
                     alignment: "center",
@@ -305,7 +305,7 @@ async function generatePDF(download = true) {
                     text: "Podpis Wykonawcy lub osoby upoważnionej",
                     alignment: "center",
                     fontSize: 10,
-                    margin: [0, 0, 0, 0],
+                    // margin: [0, 0, 0, 0],
                   },
                 ],
               },
